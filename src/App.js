76c495905422c12b6c4d89de07a3871a679926a4
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-01-09 22:53:06 +0100

    dispatch action

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import Answer from "./components/Answer/Answer";
 import Faq from "./components/Faq/Faq";
 import classes from "./App.module.css";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import useFetch from "./hooks/use-fetch";
 
 function App() {
   const idxFromStore = useSelector((state) => state.sort.selectedIdx);
 
   const faqIsVisible = useSelector((state) => state.faq.faqIsVisible);
-  const { fetchJoke } = useFetch();
+  const { fetchJoke, loading } = useFetch();
 
-  const duplicateId = idxFromStore.filter(
-    (el, id, arr) => arr.indexOf(el) !== id
-  );
-  if (duplicateId.length > 0) {
-    console.log("powtórzenie");
-  }
+  useEffect(() => {
+    if (loading) {
+      const duplicateId = idxFromStore.filter(
+        (el, id, arr) => arr.indexOf(el) !== id
+      );
+      if (duplicateId.length > 0) {
+        console.log("powtórzenie");
+      }
+    }
+  }, [fetchJoke]);
 
   useEffect(() => {
     fetchJoke();
--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import { questionActions } from "../store/question-slice";
 import { sortActions } from "../store/sort-slice";
 
 const useFetch = () => {
+  const [loading, setIsLoading] = useState();
   const dispatch = useDispatch();
   const idxFromStore = useSelector((state) => state.sort.selectedIdx);
 
-  const fetchJoke = async () => {
-    const randomIdx = Math.floor(Math.random() * 5) + 1;
-
+  if (loading) {
     const duplicateId = idxFromStore.filter(
       (el, id, arr) => arr.indexOf(el) !== id
     );
     if (duplicateId.length > 0) {
       console.log("powtórzenie");
     }
+  }
+
+  const fetchJoke = async () => {
+    setIsLoading(true);
+    const randomIdx = Math.floor(Math.random() * 3) + 1;
 
     try {
       const response = await fetch(
@@ -33,9 +37,6 @@ const useFetch = () => {
 
       const responseData = await response.json();
 
-      // console.log(idxFromStore);
-      // console.log(responseData);
-
       dispatch(
         questionActions.getRandomJoke({
           answer: responseData.answer,
@@ -49,11 +50,8 @@ const useFetch = () => {
     }
   };
 
-  // useEffect(() => {
-  //   fetchJoke();
-  // }, []);
-
-  return { fetchJoke };
+  return { fetchJoke, loading };
+  setIsLoading(false);
 };
 
 export default useFetch;
